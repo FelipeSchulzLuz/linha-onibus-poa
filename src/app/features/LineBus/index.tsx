92ec06e5c9b3a4cc6fd3d36e8922080aead815be
Felipe Schulz <Schulz.neo@gmail.com>
2021-09-26 23:06:07 -0300

    remove non used

--- src/app/features/LineBus/index.tsx
+++ src/app/features/LineBus/index.tsx
@@ -1,20 +1,16 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import style from "./style.module.scss"
 import { IBus } from "../../../core/models/IBus.model";
 import loadLinesList, { loadLongLat } from "../../../core/services/busLine.service";
 import Switch from '@material-ui/core/Switch';
 import BusContext from "../../../core/store/store";
-import classNames from "classnames";
 
 export default function Index() {
     const [data, setData] = useState([]);
     const [dataFilter, setDataFilter] = useState([]);
     const [search, setSearch] = useState('');
     const [isBus, setIsBus] = useState(true);
-    const activeLine = useRef(null);
-    const [active, setActive] = useState(false);
-
-    console.log(activeLine);
+    // const [isActive, setActive] = useState(false);
 
     const { setCoords } = useContext(BusContext);
 
@@ -31,9 +27,14 @@ export default function Index() {
     }
 
     function handleClick(event: any, id: string) {
+        event.currentTarget.classList.add(style.active);
+
         loadLongLat(id).then((res) => {
             setCoords(res)
         })
+    }
+    function handleBlur(event: any) {
+        event.currentTarget.classList.remove(style.active);
     }
 
     function handleSearchChange(event: any) {
@@ -52,17 +53,24 @@ export default function Index() {
             <div className={style.col}>
                 <input value={search} className={style.item} type="search" placeholder="Buscar" onChange={handleSearchChange} />
             </div>
-            <div className={classNames(style.row, style.header)}>
+            <div className={style.header}>
                 <div className="col">Nome</div>
                 <div className="col">Linha</div>
             </div>
             {dataFilter.map((item: IBus) => (
-                <div key={item.id} className={style.row} onClick={(e) => { handleClick(e, item.id) }}>
+                <div key={item.id} className={style.row}
+                    onClick={(e) => {
+                        handleClick(e, item.id)
+                        // setActive(true)
+                    }}
+                >
+
                     <div className={style.item} >{item.nome}</div>
                     <div className={style.item} >{item.codigo}</div>
                 </div>
-            ))}
-        </div>
+            ))
+            }
+        </div >
     );
 }
 
